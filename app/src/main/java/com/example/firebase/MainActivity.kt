24e943a7f6be795d5ad5e72742d68149d3e25926
Saptarshi Das <saptarshi@Saptarshis-MacBook-Pro.local>
2023-04-11 09:57:10 +0530

    firebase first

---
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.database.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var editText : EditText
+    lateinit var buttonSend : Button
+    lateinit var textViewName : TextView
+
+    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+    val reference : DatabaseReference = database.reference.child("Users")
+    val reference2 : DatabaseReference = database.reference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editTextName)
+        buttonSend = findViewById(R.id.buttonSend)
+        textViewName = findViewById(R.id.textViewName)
+
+        reference2.addValueEventListener( object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val realName : String = snapshot.child("Users").child("name").value as String
+                textViewName.text = realName
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+        buttonSend.setOnClickListener{
+            val userName= editText.text.toString()
+            reference.child("userName2").setValue(userName)
+
+
+        }
+    }
+}
